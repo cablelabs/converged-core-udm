72261719f575c138d9cddad5ed298615d100f4e3
dschrimpsher <d.schrimpsher@cablelabs.com>
2019-04-19 13:51:05 -0500

    Basic start. Run by doing python udm_api/app.py
    Notes:
    1.  I had to make the $ref absolute path with sucks but there is a lot of debate going on on connexion (and other places) on the best way to use multiple files.
    
    2.  The functions don't do anything yet and I am not sure excalyt how to get a token since the oauth references a api/yaml file file I don't have.
    
    I used a virtual env in pycharm.  So you may need to do a pip install or whatever you normally do and get all the libraries.  I will (soon) setup a requirements, setup.cfg and such so that goes away but ...

---
+++ udm_api/app.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import datetime
+import logging
+
+import connexion
+from connexion import NoContent
+
+
+
+def getSupiById(id):
+    return NoContent, 200
+
+
+def getSupiNssai():
+    return NoContent, 200
+
+
+def getSupiAmData():
+    return NoContent, 200
+
+
+def postSupiSdmSubscriptions():
+    return NoContent, 200
+
+
+def deleteSupiSubscriptionById():
+    return NoContent, 200
+
+
+def putRegistrationAmfNon3gppAccess():
+    return NoContent, 200
+
+
+logging.basicConfig(level=logging.INFO)
+app = connexion.App(__name__, specification_dir='openapi/')
+# app.add_api('TS29503_Nudm_UECM.yaml')
+app.add_api('TS29503_Nudm_SDM.yaml')
+
+# set the WSGI application callable to allow using uWSGI:
+# uwsgi --http :8080 -w app
+application = app.app
+
+if __name__ == '__main__':
+    # run our standalone gevent server
+    app.run(port=8080, server='gevent')
